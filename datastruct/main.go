1d3b1ef05a8111791a090b72e9a7252c523f969d
Priyanka-bal <111605538+Priyanka-bal@users.noreply.github.com>
2024-09-05 13:18:53 +0530

    datastructure

---
+++ datastruct/main.go
@@ -0,0 +1,26 @@
+package main
+
+//import "fmt"
+
+func main() {
+	var nums[4]int   
+	// a := 1
+	// b := 1
+	// c := 1
+	// var p, q, r, s, t, u, v int
+	// a = 10
+	// var list [10]int
+	// list[0] = 10
+	// list[1] = 11
+	// list[2] = 12
+	// list[3] = 13
+	// list[4] = 14
+	// list[5] = 15
+	// list[6] = 16
+	// list[7] = 17
+	// list[8] = 18
+	// list[9] = 19
+	// fmt.Println("List=",list)
+	// list[5]=454
+	// fmt.Println("List=",list)
+}
